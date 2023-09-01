3e85ec24ef635233233b9138c85eb84a936da1bc
Loyoenock <loyoenock@gmail.com>
2023-09-01 12:19:33 +0300

    Project initial setup added

---
+++ index.php
@@ -0,0 +1,242 @@
+<?php
+include 'partials/header.php'
+?>
+
+
+<section class="featured">
+    <div class="container featured__container">
+        <div class="post__thumbnail">
+            <img src="/images/blog1.jpg">
+        </div>
+        <div class="post__info">
+            <a href="category-post.php" class="category__button">Jewish Traditions and Rituals</a>
+            <h2 class="post__title"><a href="post.php">The Significance of Hanukkah: A Festival of Lights</a></h2>
+            <p class="post__body">Hanukkah, also known as the Festival of Lights, commemorates the rededication of the
+                Holy Temple in Jerusalem and the miracle of the oil that lasted for eight days. This celebration holds
+                deep spiritual significance, reminding us to bring light into our lives and stand against oppression. As
+                families light the menorah, spin the dreidel, and enjoy traditional foods, they honor the resilient
+                spirit of the Hebrew people throughout history.</p>
+
+            <div class="post__author">
+                <div class="post__author-avatar">
+                    <img src="/images/avatar2.jpg">
+                </div>
+                <div class="posSarah Cohent__author-info">
+                    <h5>By: Sarah Cohen</h5>
+                    <small>June 29, 23</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--========================== End of Featured ==========================-->
+
+
+<section class="posts">
+    <div class="container post__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog3.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Jewish Traditions and Rituals</a>
+                <h3 class="post__title">
+                    <a href="post.php">The Significance of Hanukkah: A Festival of Lights.</a>
+                </h3>
+                <p class="post__body">Explore the rich history and symbolism behind Hanukkah, a festival that celebrates
+                    the triumph of light over darkness.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar4.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Sarah Cohen</h5>
+                        <small>Date: June 29, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog4.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Torah Study and Commentary</a>
+                <h3 class="post__title">
+                    <a href="post.php">Unveiling Genesis: Exploring the Creation Narrative</a>
+                </h3>
+                <p class="post__body">Journey through the opening chapters of Genesis and uncover layers of meaning in
+                    the creation story.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar5.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Rabbi Jacob Cohen</h5>
+                        <small>Date: April 3, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog5.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Jewish Traditions and Rituals</a>
+                <h3 class="post__title">
+                    <a href="post.php">Passover Seder: A Feast of Liberation and Unity.</a>
+                </h3>
+                <p class="post__body">Delve into the rituals and symbolism of the Passover Seder, a time-honored
+                    tradition that retells the story of Exodus.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar6.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: David Levy</h5>
+                        <small>Date: August 14, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog6.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Torah Study and Commentary</a>
+                <h3 class="post__title">
+                    <a href="post.php">Unpacking Exodus: The Journey to Liberation</a>
+                </h3>
+                <p class="post__body">LExamine the Exodus story as a universal tale of liberation, resilience, and the
+                    pursuit of freedom.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar7.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Dr. Rebecca Stein</h5>
+                        <small>Date: May 18, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog7.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Ethical and Moral Discussions</a>
+                <h3 class="post__title">
+                    <a href="post.php">The Ethics of Compassion: Lessons from Jewish Tradition</a>
+                </h3>
+                <p class="post__body">Delve into the teachings of Jewish ethics and discover how compassion lies at the
+                    heart of moral decision-making.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar8.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Rabbi Rachel Stern</h5>
+                        <small>Date: July 15, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog8.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Personal Stories and Reflections</a>
+                <h3 class="post__title">
+                    <a href="post.php">Embracing My Jewish Heritage: A Journey of Rediscovery</a>
+                </h3>
+                <p class="post__body">Join me on a personal journey of reconnecting with my Jewish heritage and the
+                    impact it has had on my identity.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar9.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Sarah Cohen</h5>
+                        <small>Date: December 1, 2022</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog9.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Family and Parenting</a>
+                <h3 class="post__title">
+                    <a href="post.php">Passing Down Traditions: Nurturing Hebrew Identity in Children</a>
+                </h3>
+                <p class="post__body">Discover the joys and challenges of instilling Hebrew traditions and values in the
+                    younger generation.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar10.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Leah Abrams</h5>
+                        <small>Date: January 8, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="/images/blog9.jpg">
+            </div>
+            <div class="post__info">
+                <a href="" class="category__button">Current Events</a>
+                <h3 class="post__title">
+                    <a href="post.php">Navigating Faith Amidst Change: Hebrew Community in a Modern World</a>
+                </h3>
+                <p class="post__body">Explore how the Hebrew community adapts to contemporary challenges and changes
+                    while remaining rooted in faith.</p>
+                <div class="post__author">
+                    <div class="post__author-avatar">
+                        <img src="/images/avatar10.jpg" alt="">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>By: Leah Abrams</h5>
+                        <small>Date: January 8, 2023</small>
+                    </div>
+                </div>
+            </div>
+        </article>
+
+
+</section>
+<!--========================== End of Post section ==========================-->
+
+
+
+<section class="category__buttons">
+    <div class="container category__buttons-container">
+        <a href="" class="category__button">Torah Study and Commentary</a>
+        <a href="" class="category__button">Hebrew Traditions and Rituals</a>
+        <a href="" class="category__button">Personal Stories and Reflections</a>
+        <a href="" class="category__button">Ethical and Moral Discussionsy</a>
+        <a href="" class="category__button">Family and Parenting</a>
+        <a href="" class="category__button">Current Events </a>
+    </div>
+</section>
+<!--========================== End of Category button ==========================-->
+
+
+<?php
+include 'partials/footer.php'
+?>
---
+++ post.php
@@ -0,0 +1,42 @@
+<?php
+include 'partials/header.php'
+?>
+
+<section class="singlepost">
+    <div class="container singlepost__container">
+        <h2>The Significance of Hanukkah: A Festival of Lights</h2>
+        <div class="post__author">
+            <div class="post__author-avatar">
+                <img src="/images/avatar2.jpg">
+            </div>
+            <div class="post__author-info">
+                <h5>By: Sarah Cohen</h5>
+                <small>June 29, 23</small>
+            </div>
+        </div>
+
+        <div class="singlepost__thumbnail">
+            <img src="./images/blog33.jpg">
+        </div>
+        <p>Explore the rich history and symbolism behind Hanukkah, a festival that celebrates the triumph of light over
+            darkness.</p>
+
+        <p>Hanukkah, also known as the Festival of Lights, commemorates the rededication of the Holy Temple in Jerusalem
+            and the miracle of the oil that lasted for eight days. This celebration holds deep spiritual significance,
+            reminding us to bring light into our lives and stand against oppression. As families light the menorah, spin
+            the dreidel, and enjoy traditional foods, they honor the resilient spirit of the Hebrew people throughout
+            history.
+        </p>
+        <p>On a chilly winter's evening, Jewish households around the world gather to celebrate Hanukkah. This festival,
+            spanning eight days and nights, carries a profound message of hope and perseverance. The story behind
+            Hanukkah dates back to the time of the Maccabean Revolt when a small group of Jewish warriors reclaimed the
+            Holy Temple in Jerusalem from the oppressive rule of King Antiochus IV</p>
+    </div>
+    <!--========================== End of Single post =====================-->
+
+
+</section>
+
+<?php
+include 'partials/footer.php'
+?>

a416fe968e0dc2db8088766485296c894f67ceac
Loyoenock <loyoenock@gmail.com>
2023-09-01 12:31:06 +0300

    Header and footer files added

---
+++ partials/footer.php
@@ -0,0 +1,65 @@
+<footer>
+    <div class="footer__socials">
+        <a href=""><i class="uil uil-youtube"></i></a>
+        <a href=""><i class="uil uil-facebook"></i></i></a>
+        <a href=""><i class="uil uil-twitter"></i></a>
+        <a href=""><i class="uil uil-linkedin"></i></a>
+        <a href=""><i class="uil uil-instagram-alt"></i></a>
+    </div>
+
+    <div class="container footer__container">
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Torah Study and Commentary</a></li>
+                <li><a href="">Hebrew Traditions and Rituals</a></li>
+                <li><a href="">Personal Stories and Reflections</a></li>
+                <li><a href="">Ethical and Moral Discussionsy</a></li>
+                <li><a href="">Family and Parenting</a></li>
+                <li><a href="">Current Events</a></li>
+            </ul>
+        </article>
+
+        <article>
+            <h4>Support</h4>
+            <ul>
+                <li><a href="">Online support</a></li>
+                <li><a href="">Call Numbers</a></li>
+                <li><a href="">Emails</a></li>
+                <li><a href="">Social Support</a></li>
+                <li><a href="">Location</a></li>
+            </ul>
+        </article>
+
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="">Hebrew Community in a Modern World</a></li>
+                <li><a href="">A Journey of Rediscovery</a></li>
+                <li><a href="">The Journey to Liberation</a></li>
+                <li><a href="">Exploring the Creation Narrative</a></li>
+                <li><a href="">A Festival of Lights</a></li>
+            </ul>
+        </article>
+
+        <article>
+            <h4>Permalinks</h4>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </article>
+    </div>
+
+    <div class="footer__copyright">
+        <small class="footer__copyright">Copyright 2023 &copy; Loyofication</small>
+    </div>
+</footer>
+<!---------Js-------->
+<script src="<?= ROOT_URL ?>script/main.js"></script>
+</body>
+
+</html>
